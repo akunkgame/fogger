76cf8867e1e15302503d42f35abeb38c7cecaefc
akunkgame <113031777+akunkgame@users.noreply.github.com>
2024-03-15 21:24:07 +0700

    Add files via upload

---
+++ foggers/tangible heritage (kebudayaan benda.js
@@ -0,0 +1,11 @@
+tangible heritage (kebudayaan bendawi yang dapat dilihat, diraba, dan dirasa)
+Seni Budaya, Permainan, olahraga, Benda benda, Cagar Budaya
+Reog https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FReog&psig=AOvVaw2zk23F9cvhuyjpy4anP6Yz&ust=1703329558599000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJimuJ3zooMDFQAAAAAdAAAAABAD
+Rumah https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DzDQWJppc010&psig=AOvVaw2uxRPmfvquoYKKPAr4_eqL&ust=1703316411634000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIj7kaDCooMDFQAAAAAdAAAAABAH
+Karapan sapi https://www.silontong.com/2018/10/27/permainan-olahraga-tradisional-jawa-timur/info-mengenai-permainan-karapan-sapi-jawa-timur-dan-sejarahnya/
+Makanaan https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.traveloka.com%2Fid-id%2Fexplore%2Fculinary%2Fpl-makanan-khas-jawa-timur%2F159306&psig=AOvVaw0aRsYAm7KKB6wRBvDEuSrO&ust=1703317382010000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMDntfHFooMDFQAAAAAdAAAAABAI
+wayang https://www.indonesia.travel/content/dam/indtravelrevamp/id-id/ide-liburan/diakui-sebagai-warisan-unesco-ini-5-hal-menarik-seputar-wayang-kulit/thumbnail.jpg
+
+intangible heritage (kebudayaan tak berdawi bersifat abstrak dan lebih sulit dipahami)
+Aksara, bahasa, adat istiadat, norma, etika ,spiritualitas, teknologoi tradisional
+Aksara https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.detik.com%2Fedu%2Fdetikpedia%2Fd-6000300%2F6-julukan-ragam-aksara-di-dunia-siswa-sudah-tahu&psig=AOvVaw28bTBt7bmImOcCi1Xq63HE&ust=1703322685655000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMD51dLZooMDFQAAAAAdAAAAABAK
